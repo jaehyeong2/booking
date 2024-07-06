bb3cf87a26c2f783d01a63929e4f680503d19481
jaehyeong <wogud1514@gmail.com>
2024-07-06 18:07:51 +0900

    chore: resilience4j-circuitbreaker

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	implementation("io.github.resilience4j:resilience4j-circuitbreaker:2.2.0")
+
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
